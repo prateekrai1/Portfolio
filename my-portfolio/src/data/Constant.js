18ca4429115175c9fdc47e65d4974eb38cce158e
prateekrai1 <prateek@gmail.com>
2023-12-25 17:03:02 -0500

    Hero section done

--- my-portfolio/src/data/Constant.js
+++ my-portfolio/src/data/Constant.js
@@ -2,8 +2,9 @@ export const Bio = {
     name: "Prateek Rai Srivastav",
     roles: [
       "Full Stack Developer",
-      "UI/UX Designer",
+      "Java Backend Developer",
       "Programmer",
+      ""
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
