09dded4b347f3c2cded48696fc58d35de6ac0e34
prateekrai1 <prateek@gmail.com>
2023-12-26 21:12:30 -0500

    Completed

--- my-portfolio/src/components/Footer/index.js
+++ my-portfolio/src/components/Footer/index.js
@@ -97,13 +97,10 @@ function Footer() {
           <NavLink href="#education">Education</NavLink>
         </Nav>
         <SocialMediaIcons>
-          <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon>
-          <SocialMediaIcon href={Bio.twitter} target="display"><TwitterIcon /></SocialMediaIcon>
           <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon>
-          <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Rishav Chanda. All rights reserved.
+          &copy; 2023 Prateek Rai Srivastav. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
--- my-portfolio/src/data/Constant.js
+++ my-portfolio/src/data/Constant.js
@@ -11,7 +11,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/prateekrai1",
     resume:
-      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+      "https://drive.google.com/file/d/1K7q_O9CAslaGRjSXoSi9a8Qn9Mgyud4y/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/prateek-rai-50001821a/",
   };
   
